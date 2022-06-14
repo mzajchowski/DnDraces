6021cf892fe70b43818c380a44e5b1f50efe17a8
Michał Zajchowski <84908332+mzajchowski@users.noreply.github.com>
2022-06-14 22:19:15 +0200

    Update RaceName.scala

--- src/main/scala/com/main/RaceName.scala
+++ src/main/scala/com/main/RaceName.scala
@@ -1,13 +1,3 @@
 package com.main
 
-enum RaceName(raceName: String) {
-  case Dragonborn extends RaceName("Dragoborn")
-  case Dwarf extends RaceName("Dwarf")
-  case Elf extends RaceName("Elf")
-  case Gnome extends RaceName("Gnome")
-  case Halfelf extends RaceName("Half-Elf")
-  case Halfling extends RaceName("Halfling")
-  case Halforc extends RaceName("Half-Orc")
-  case Human extends RaceName("Human")
-  case Tiefling extends RaceName("Tiefling")
-}
+case class RaceName(raceName: Str

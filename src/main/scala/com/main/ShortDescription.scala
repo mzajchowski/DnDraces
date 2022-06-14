80c357233bc6ad762760c56a27cf043e0992bf3f
Michał Zajchowski <84908332+mzajchowski@users.noreply.github.com>
2022-06-14 22:19:44 +0200

    Update ShortDescription.scala

--- src/main/scala/com/main/ShortDescription.scala
+++ src/main/scala/com/main/ShortDescription.scala
@@ -1,14 +1,3 @@
 package com.main
 
-enum ShortDescription(shortDescription: String) {
-  case Dragonborn extends ShortDescription("Dragonborn look very much like dragons standing erect in humanoid form, though they lack wings or a tail.")
-  case Dwarf extends ShortDescription("Bold and hardy, dwarves are known as skilled warriors, miners, and workers of stone and metal.")
-  case Elf extends ShortDescription("Elves are a magical people of otherworldly grace, living in the world but not entirely part of it.")
-  case Gnome extends ShortDescription("A gnome's energy and enthusiasm for living shines through every inch of his or her tiny body.")
-  case Halfelf extends ShortDescription("Half-elves combine what some say are the best qualities of their elf and human parents.")
-  case Halfling extends ShortDescription("The diminutive halflings survive in a world full of larger creatures by avoiding notice or, barring that, avoiding offense.")
-  case Halforc extends ShortDescription("Some half-orcs rise to become proud leaders of orc communities. Some venture into the world to prove their worth. Many of these become adventurers, achieving greatness for their mighty deeds.")
-  case Human extends ShortDescription("Humans are the most adaptable and ambitious people among the common races. Whatever drives them, humans are the innovators, the achievers, and the pioneers of the worlds.")
-  case Tiefling extends ShortDescription("To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye: this is the lot of the tiefling.")
-
-}
+case class ShortDescription(shortDescription: String)
